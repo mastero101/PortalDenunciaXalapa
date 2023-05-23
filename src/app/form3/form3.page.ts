b5ac8deba594c3e291011a97471a7f1de4647684
mastero <castro.alejandro17@gmail.com>
2023-05-23 03:10:20 -0600

    new methos for Enviar button failed

--- src/app/form3/form3.page.ts
+++ src/app/form3/form3.page.ts
@@ -14,6 +14,8 @@ export class Form3Page implements OnInit {
   showFormFields: boolean = false;
   showFormFields2: boolean = false;
   estatura: number = 1.40;
+  mostrarConfirmacion: boolean = true;
+  mostrarConfirmacionExitosa: boolean = false; 
 
   constructor(private router: Router) {
     this.selectedOption = '';
@@ -40,6 +42,43 @@ export class Form3Page implements OnInit {
 
   toggleFormFields2(event: any) {
     this.showFormFields2 = event.detail.checked;
+  }
+
+  enviarDenuncia() {
+    // Aquí puedes realizar las acciones necesarias para enviar la denuncia
+    // por ejemplo, realizar una petición HTTP al servidor
+  
+    // Después de enviar la denuncia exitosamente, mostramos la pantalla de confirmación exitosa
+    this.mostrarConfirmacion = false;
+    this.mostrarConfirmacionExitosa = true;
+  }
+
+  cancelarEnvio() {
+    // Aquí puedes realizar las acciones necesarias al cancelar el envío de la denuncia
+    // por ejemplo, restablecer los campos del formulario
+  
+    // Volvemos a mostrar la pantalla de confirmación
+    this.mostrarConfirmacion = true;
+    this.mostrarConfirmacionExitosa = false;
+  }
+  
+  // Función para realizar otra denuncia
+  realizarOtraDenuncia() {
+    // Aquí puedes realizar las acciones necesarias para iniciar una nueva denuncia
+  
+    // Volvemos a mostrar la pantalla de confirmación
+    this.mostrarConfirmacion = true;
+    this.mostrarConfirmacionExitosa = false;
+  }
+  
+  // Función para ir a la página de inicio
+  irAInicio() {
+    // Aquí puedes realizar las acciones necesarias para navegar a la página de inicio
+  }
+  
+  // Función para ver el folio de la denuncia
+  verFolio() {
+    // Aquí puedes realizar las acciones necesarias para mostrar el folio de la denuncia
   }
 
 }
