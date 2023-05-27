7aa39911772657144290594d6f001f05d3e9f513
mastero <castro.alejandro17@gmail.com>
2023-05-27 13:09:48 -0600

    Update CORS Policy for access from every ip adress

--- CRUD/server.js
+++ CRUD/server.js
@@ -17,7 +17,7 @@ const connection = mysql.createConnection(dbConfig);
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
